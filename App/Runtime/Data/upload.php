7c4b4f5c1c90f03e71806b1e90b831fbf1330b80
yangweijie <917647288@qq.com>
2015-03-23 13:24:00 +0800

    添加uploadify c插件

---
+++ App/Runtime/Data/upload.php
@@ -0,0 +1,18 @@
+<?php	return array ( 'mimes' => '', 'maxSize' => 5242880, 'exts' => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', 'autoSub' => true, 'subName' => array ( 0 => 'date', 1 => 'Y-m-d', ), 'rootPath' => './Uploads/file/', 'savePath' => '', 'saveName' => array ( 0 => 'uniqid', 1 => '', ), 'saveExt' => '', 'replace' => false, 'hash' => true, 'callback' => false, );?>
+<?php	return false;?>
+<?php	return array ( 'status' => 0, 'info' => '上传文件大小不符！', 'data' => '', );?>
+<?php	return array ( 'mimes' => '', 'maxSize' => 5242880, 'exts' => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', 'autoSub' => true, 'subName' => array ( 0 => 'date', 1 => 'Y-m-d', ), 'rootPath' => './Uploads/file/', 'savePath' => '', 'saveName' => array ( 0 => 'uniqid', 1 => '', ), 'saveExt' => '', 'replace' => false, 'hash' => true, 'callback' => false, );?>
+<?php	return false;?>
+<?php	return array ( 'status' => 0, 'info' => '上传文件大小不符！', 'data' => '', );?>
+<?php	return array ( 'mimes' => '', 'maxSize' => 5242880, 'exts' => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', 'autoSub' => true, 'subName' => array ( 0 => 'date', 1 => 'Y-m-d', ), 'rootPath' => './Uploads/file/', 'savePath' => '', 'saveName' => array ( 0 => 'uniqid', 1 => '', ), 'saveExt' => '', 'replace' => false, 'hash' => true, 'callback' => false, );?>
+<?php	return false;?>
+<?php	return array ( 'status' => 0, 'info' => '上传文件大小不符！', 'data' => '', );?>
+<?php	return array ( 'mimes' => '', 'maxSize' => 5242880, 'exts' => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', 'autoSub' => true, 'subName' => array ( 0 => 'date', 1 => 'Y-m-d', ), 'rootPath' => './Uploads/file/', 'savePath' => '', 'saveName' => array ( 0 => 'uniqid', 1 => '', ), 'saveExt' => '', 'replace' => false, 'hash' => true, 'callback' => false, );?>
+<?php	return false;?>
+<?php	return array ( 'status' => 0, 'info' => '上传文件大小不符！', 'data' => '', );?>
+<?php	return array ( 'mimes' => '', 'maxSize' => 5242880, 'exts' => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', 'autoSub' => true, 'subName' => array ( 0 => 'date', 1 => 'Y-m-d', ), 'rootPath' => './Uploads/file/', 'savePath' => '', 'saveName' => array ( 0 => 'uniqid', 1 => '', ), 'saveExt' => '', 'replace' => false, 'hash' => true, 'callback' => false, );?>
+<?php	return false;?>
+<?php	return array ( 'status' => 0, 'info' => '上传文件大小不符！', 'data' => '', );?>
+<?php	return array ( 'mimes' => '', 'maxSize' => 5242880, 'exts' => 'jpg,gif,png,jpeg,zip,rar,tar,gz,7z,doc,docx,txt,xml', 'autoSub' => true, 'subName' => array ( 0 => 'date', 1 => 'Y-m-d', ), 'rootPath' => './Uploads/file/', 'savePath' => '', 'saveName' => array ( 0 => 'uniqid', 1 => '', ), 'saveExt' => '', 'replace' => false, 'hash' => true, 'callback' => false, );?>
+<?php	return false;?>
+<?php	return array ( 'status' => 0, 'info' => '上传文件大小不符！', 'data' => '', );?>
