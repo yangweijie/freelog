1e9dd1e87f7179e2b22982afb264ca8028a085b7
yangweijie <917647288@qq.com>
2015-03-28 16:48:34 +0800

    测试分页

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -2,8 +2,20 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-    	$this->assign('list', D('Post')->select());
+    public function index($page = 1){
+    	$map = array();
+    	$list_row = I('r', 1);
+    	$this->assign('list', D('Post')->page($page, $list_row)->select());
+		/* 分页 */
+		$total = M('Post')->where($map)->count();
+		$page = new \Think\Page($total, $list_row);
+		$page->rollPage = 5;
+		$page->setConfig('prev','上一页');
+		$page->setConfig('next','下一页');
+		$page->setConfig('theme','<div class="pager">%UP_PAGE% %DOWN_PAGE%</div>');
+
+		$p = $page->show();
+		$this->assign('_page', $p? $p: '');
     	$this->display();
     }
 }
