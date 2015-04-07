a73497a105d93fb419cc7557e838fa32890c7303
yangweijie <917647288@qq.com>
2015-04-07 08:19:46 +0800

    修复错误

--- App/Common/Model/MemberModel.class.php
+++ App/Common/Model/MemberModel.class.php
@@ -9,6 +9,7 @@ class MemberModel extends Model{
 
 	protected $_auto = array (
 		array('status','1'),  // 新增的时候把status字段设置为1
+		array('setting', '{}'),
 		array('pwd','password',1,'function') , // 对password字段在新增的时候使md5函数处理
 		array('create_at','datetime',self::MODEL_INSERT,'function'), // 对create_at字段在更新的时候写入当前时间戳
 		array('update_at','datetime',self::MODEL_BOTH,'function'), // 对create_time字段在更新的时候写入当前时间戳
